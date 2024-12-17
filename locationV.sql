a2e2bcea7f29fbed1ad6caf97a9859fc6708cd96
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-17 21:59:37 +0100

    Création de la table utilisateur

--- locationV.sql
+++ locationV.sql
@@ -4,4 +4,13 @@ CREATE DATABASE location_voiture;
 
 USE  location_voiture;
 
+CREATE TABLE utilisateur(
+  id_user INT PRIMARY KEY AUTO_INCREMENT,
+  nom_user VARCHAR(255) NOT NULL,
+  prenom_user VARCHAR(255) NOT NULL,
+  email_user VARCHAR(255) NOT NULL,
+  tel_user  VARCHAR(15) NOT NULL,
+  role_user ENUM('client','admin') NOT NULL
+);
+
 
