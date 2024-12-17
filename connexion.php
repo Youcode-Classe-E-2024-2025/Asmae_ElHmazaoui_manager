ac597596c8cf4b39730576ed4b5288ddab8dc7fa
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-17 23:00:55 +0100

    laison entre la page connexion.php et connexion.css

--- connexion.php
+++ connexion.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
+    <link rel="stylesheet" href="asset/connexion.css">
 </head>
 <body>
     <div class="form-container">
