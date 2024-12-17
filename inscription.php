5cce43bf6ccb6dfdcdaec46c553bd0c632206622
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-17 22:47:14 +0100

    insertion des élements de la page inscription.php

---
+++ inscription.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+
+    <div class="form-container">
+        <h2>Inscription</h2>
+        <form action="#" method="POST">
+            <div>
+                <label >Nom :</label>
+                <input type="text"  name="nom_user" required>
+            </div>
+            <div>
+                <label>Prénom :</label>
+                <input type="text"name="prenom_user" required>
+            </div>
+            <div>
+                <label>Email :</label>
+                <input type="email"name="email_user" required>
+            </div>
+            <div>
+                <label>Numéro de téléphone :</label>
+                <input type="tel" name="tel_user" pattern="[0-9]{10}" required>
+            </div>
+            <button type="submit">Envoyer</button>
+        </form>
+
+        <!-- Liens pour la connexion -->
+        <div class="link-container">
+            <p>Vous avez déjà un compte ? <a href="login.html">Connexion</a></p>
+        </div>
+    </div>
+
+</body>
+</html>
