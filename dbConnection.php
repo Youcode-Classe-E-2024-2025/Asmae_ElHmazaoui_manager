f9d3e146ccf589cbc06b0588d52fca40528c442a
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-18 09:17:55 +0100

    établir la connexion avec la base de donnée location_voiture

---
+++ dbConnection.php
@@ -0,0 +1,27 @@
+<?php
+
+// définir arguments de consrtucteur de la classe mysqli
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="location_voiture";
+
+// création de l'objet $conn à partir de la class mysqli
+
+$conn= new mysqli($servername,$username,$password,$dbname);
+
+// check de la connexion;
+
+if($conn->connect_error){
+    die("connexion impossible" .$conn->connect_error);
+}
+else{
+    echo "connexion réalisée";
+}
+
+
+
+
+
+
+?>
