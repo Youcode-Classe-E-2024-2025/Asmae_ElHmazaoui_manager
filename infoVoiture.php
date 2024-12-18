60c2bbfc6659b0be0fb412cb3fb6526729fb8acc
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-18 01:28:11 +0100

    styler le formulaire d'ajout d'une voiture avec le fichier infoVoiture.css

--- infoVoiture.php
+++ infoVoiture.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajouter une voiture</title>
-    <link style="stylesheet" href="asset/infoVoiture.php">
+    <link rel="stylesheet" href="asset/infoVoiture.css">
 </head>
 <body>
 
