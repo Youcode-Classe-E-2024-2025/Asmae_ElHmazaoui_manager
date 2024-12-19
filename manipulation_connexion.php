3f248d24574513ad5d9a51e0fa38882b00cd5711
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-19 13:00:22 +0100

    manipulation des données de la page connexion.php dans la page manipulation_connexion.php

--- manipulation_connexion.php
+++ manipulation_connexion.php
@@ -3,12 +3,37 @@
 
    if($_SERVER('REQUEST_METHOD')== 'POST'){
     
+      $email=htmlspecialchars($_POST['email_user']);
+      $password_user= htmlspecialchars($_POST['password_user']);
 
+     $sql=$conn->prepare("SELECT * FROM utilisateur WHERE  email_user = ?");
+     $sql->bind_param("s",$email);
+     $sql->execute();
+     $result= $sql->get_result();
+     
+     if($result->nun_row>0){
+        
+        $user_C=$result->fetch_assoc();
 
+           // chech si le mot de passe saisi c'est exactemnt qui existe dans la base de donnée
+           if(password_verify($password_user,$user_C['password_user'])){
+            
+                //  redirection selon le role de l'utilisateur 
+                  if($user_C[' role_user'] == 'admin'){
+                    header('Location : tok.php');
+                  }
+                  else{
+                    header('Location : tok3.php');
+                  }
+           }else{
+            echo"mot de passe incorrecte";
+           }
 
-
-
-
+     }else{
+        echo"email non trouvé";
+     }
+ $sql->close();
+ $conn->close();
 
    }
 
