c063fd197904204baceb2a5103cdf0d3c46bc9fa
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-19 16:33:15 +0100

    validation des inputs de formulaire de la voiture

--- manipulation_info_voiture.php
+++ manipulation_info_voiture.php
@@ -1,5 +1,44 @@
 <?php
 include 'dbConnection.php';
 
+if($_SERVER['REQUEST_METHOD']== 'POST'){
+    // validation pour le nom du voiture
+    $nom_voiture  = htmlspecialchars($_POST['nom_voiture']);
+         if(!preg_match("/^[a-zA-Z' ]+$/",$nom_voiture)){
+           $nomVoiture_ERR = "le nom doit contenir seulement les lettres";
+         }
+
+    // validation pour le modèle du voiture
+    $modele_voiture  = htmlspecialchars($_POST['modele']);
+    if(!preg_match("/^[A-Za-z0-9\- ]+$ /",$modele_voiture)){
+      $modeleVoiture_ERR = "le nom doit contenir seulement les lettres";
+    }
+
+     // validation pour la marque  du voiture
+     $marque_voiture  = htmlspecialchars($_POST['marque']);
+     if(!preg_match("/^[A-Za-z\-]+$/",$marque_voiture)){
+       $marqueVoiture_ERR = "le nom doit contenir seulement les lettres";
+     }
+
+      // validation du couleur de la voiture
+      $couleur_voiture  = htmlspecialchars($_POST['couleur']);
+      if(!preg_match("/^[a-zA-Z' ]+$/",$couleur_voiture)){
+        $couleurVoiture_ERR = "le nom doit contenir seulement les lettres";
+      }
+
+       // validation du prix de la voiture
+       $prix_jour_voiture  = htmlspecialchars($_POST['prix_jour']);
+       if(!filter_var($prix_jour_voiture, FILTER_VALIDATE_FLOAT) && $prix_jour_voiture > 0){
+         $prix_jourVoiture_ERR = "le nom doit contenir seulement les lettres";
+       }
+
+       // validation de la photo de la voiture
+       $photo_voiture = htmlspecialchars($_POST['photo_voiture']);
+       if(filter_var($photo_voiture, FILTER_VALIDATE_URL)){
+         $photo_voiture_ERR = "le nom doit contenir seulement les lettres";
+       }
+
+       
+}
 
 ?>
