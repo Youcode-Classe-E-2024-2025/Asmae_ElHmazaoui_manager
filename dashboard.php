db4e9333faf7a2f14944caee603ef13c1f0359aa
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-20 09:34:22 +0100

    changement du nom du fichier du dashboard et la laison avec son fichier du style

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tableau de Bord</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="asset/dashboard.css">
 </head>
 <body>
     <div class="sidebar">
