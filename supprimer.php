ee5eb659cbb73cfd78ad20da8013c50317394feb
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-21 14:37:31 +0100

    script pour la suppression du voiture

--- supprimer.php
+++ supprimer.php
@@ -1,8 +1,9 @@
 <?php
 include 'dbConnection.php';
 
-$id_USER=$_GET['id'] ?? '';
-
+$id_USER=$_GET['id'] ?? ''; // récupération de l'id de l'utilisateur 
+$id_VOITURE=$_GET['id'] ?? ''; // récupération de l'id du voiture 
+ 
 // suppression pour les utilisateurs
 if($id_USER){
     // Supprimer les paiements associés à la réservation de l'utilisateur
@@ -31,6 +32,16 @@ if($id_USER){
     $del_user->close();
 }
 
+
+
+// suppression pour les voitures
+if($id_VOITURE){
+    // Suppression de l'utilisateur
+    $del_voiture=$conn->prepare("DELETE FROM voiture WHERE id_voiture = ?");
+    $del_voiture->bind_param("i",$id_VOITURE);
+    $del_voiture->execute();
+    $del_voiture->close();
+}
 
  header('Location: dashboard.php');
 ?>
