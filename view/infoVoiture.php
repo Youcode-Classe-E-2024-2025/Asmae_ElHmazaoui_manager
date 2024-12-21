183bfedc106c2c94c861660a0311102b728d879a
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-21 22:28:30 +0100

    modification des chemins dans le fichier infoVoiture.php

--- view/infoVoiture.php
+++ view/infoVoiture.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajouter une voiture</title>
-    <link rel="stylesheet" href="asset/infoVoiture.css">
+    <link rel="stylesheet" href="../asset/infoVoiture.css">
 </head>
 <body>
 
