2d6dc572146a682861e651e143b3dd27d6e0f4d6
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-22 01:45:38 +0100

    mise à jour du style de la page inscription

--- view/inscription.php
+++ view/inscription.php
@@ -4,20 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../asset/inscription.css">
 </head>
 <body>
+<header>
+    <div class="logo">
+        <img src="../asset/vidéo/logo.png" alt="Logo">
+    </div>
+    <div class="nav-links">
+        <a href="#about-us">À propos</a>
+        <a href="#services">Nos Services</a>
+        <a href="#contact">Contact</a>
+    </div>
+    <div class="auth-icons">
+        <a href="#"><i class="fas fa-user-plus"></i></a>
+        <a href="#"><i class="fas fa-sign-in-alt"></i></a>
+    </div>
+</header>
 
     <div class="form-container">
-        <h2>Inscription</h2>
+        
         <form action="../controllers/manipulation_inscription.php" method="POST">
+        <h2>Inscription</h2>
+           <div class="name-container">
+              <div>
+                  <label>Nom :</label>
+                  <input type="text" name="nom_user" required>
+              </div>
+              <div>
+                  <label>Prénom :</label>
+                  <input type="text" name="prenom_user" required>
+              </div>
+           </div>
             <div>
-                <label >Nom :</label>
-                <input type="text"  name="nom_user" required>
-            </div>
-            <div>
-                <label>Prénom :</label>
-                <input type="text"name="prenom_user" required>
+                <label>Numéro de téléphone :</label>
+                <input type="tel" name="tel_user" pattern="[0-9]{10}" required>
             </div>
             <div>
                 <label>Email :</label>
@@ -31,18 +53,22 @@
                 <label>confirmation de mot de passe :</label>
                 <input type="password" name="confirmation_password_user" required>
             </div>
-            <div>
-                <label>Numéro de téléphone :</label>
-                <input type="tel" name="tel_user" pattern="[0-9]{10}" required>
-            </div>
+            
             <button type="submit">Envoyer</button>
+
+            <div class="link-container">
+            <p>Vous avez déjà un compte ? <a href="../view/connexion.php">Connexion</a></p>
+            </div>
         </form>
 
-        <!-- Liens pour la connexion -->
-        <div class="link-container">
-            <p>Vous avez déjà un compte ? <a href="../view/connexion.php">Connexion</a></p>
-        </div>
+      
+              
     </div>
+<!-- Image à droite -->
+       <div class="car">
+            <img src="../asset/vidéo/voitureI.png" alt="Image d'illustration">
+        </div> 
 
+     
 </body>
 </html>
