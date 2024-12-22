1a5a4d600d828dcd999c89d824d56b8ebe706b7c
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-22 16:00:22 +0100

    authentification pour l'inscription

--- controllers/manipulation_inscription.php
+++ controllers/manipulation_inscription.php
@@ -20,27 +20,48 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     // validation de confirmation pour le mot de passe 
     $confirmation_password_user  = htmlspecialchars($_POST['confirmation_password_user']);
 
+      // Vérification si les mots de passe correspondent
+       if ($password_user !== $confirmation_password_user) {
+         echo "Les mots de passe ne correspondent pas.";
+         exit;
+     }
+
+        
     // validation pour l'email 
     $email_user = htmlspecialchars($_POST['email_user']);
        
 
     // validation pour le  téléphone
     $tel_user = $_POST['tel_user'];
-         
-    
 
-    // insertions dans la base de données si les mots de passe sont identiques 
+    // authentification avant l'insertion dans la base de données 
+    $sqlCheck=$conn->prepare("SELECT email_user,tel_user FROM utilisateur WHERE role_user = 'client' AND (email_user = ? OR tel_user = ?)");
+   // lecture des paramètres
+    $sqlCheck->bind_param("ss",$email_user,$tel_user);
+   
+    // exécution de la raquete;
+    $sqlCheck->execute();
+    $resultCheck = $sqlCheck->get_result();
 
+    // Si l'email ou le téléphone existe déjà
+    if($resultCheck->num_rows >0){
+      echo"l'email ou mot de pass existe déjà ";
+      exit;
+    }
 
+    // insertions dans la base de données si les informations sont valides
       $sql=$conn->prepare("INSERT INTO utilisateur (nom_user, prenom_user,email_user,password_user,tel_user, role_user) VALUES (?,?,?,?,?,'client')");
+  
+      // lecture des paramètres pour l'insertion
       $sql->bind_param("sssss",$nom_user,$prenom_user,$email_user,$hashedPW,$tel_user);
       
+      // execution de la requete sql
       if($sql->execute()){
         header('Location: ../view/locationV.php');
       }else{
         echo "Erreur".$sql->error;
       }
-    
+   
     $sql->close();
     $conn->close();
 }
