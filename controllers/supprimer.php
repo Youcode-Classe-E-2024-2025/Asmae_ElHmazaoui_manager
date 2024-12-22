3ec16c530b22aad7ecc436963626fd4a249e713b
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2024-12-22 23:00:57 +0100

    suppression des réservations

--- controllers/supprimer.php
+++ controllers/supprimer.php
@@ -3,7 +3,10 @@ include '../model/dbConnection.php';
 
 $id_USER=$_GET['id'] ?? ''; // récupération de l'id de l'utilisateur 
 $id_VOITURE=$_GET['id'] ?? ''; // récupération de l'id du voiture 
- 
+$id_reservation=$_GET['id']?? '';
+$id_contrat=$_GET['id']?? '';
+$id_paiment=$_GET['id']?? '';
+
 // suppression pour les utilisateurs
 if($id_USER){
     // Supprimer les paiements associés à la réservation de l'utilisateur
@@ -43,5 +46,29 @@ if($id_VOITURE){
     $del_voiture->close();
 }
 
+if($id_reservation){
+
+    // Suppression  dans la table contrat
+    $sql_contrat = "DELETE FROM contrat WHERE id_contrat_res = ?";
+    $stmt = $conn->prepare($sql_contrat);
+    $stmt->bind_param("i", $id_reservation);
+    $stmt->execute();
+    $stmt->close();
+    
+    // Suppression  dans la table contrat
+    $sql_paiement = "DELETE FROM paiment WHERE id_paiment_res = ?";
+    $stmt = $conn->prepare($sql_paiement);
+    $stmt->bind_param("i", $id_reservation);
+    $stmt->execute();
+    $stmt->close();
+    
+    // Suppression dans la table reservation
+    $sql_reservation = "DELETE FROM reservation WHERE id_reservation = ?";
+    $stmt = $conn->prepare($sql_reservation);
+    $stmt->bind_param("i", $id_reservation);
+    $stmt->execute();
+    $stmt->close();
+
+}
  header('Location: ../view/dashboard.php');
 ?>
